b9dcc4c67e24167e757a33631be60618013a68ae
Tuan Kiet <tuankiet.luong@foodycorp.vn>
2020-03-17 23:38:57 +0700

    Update build script

--- mcq-frontend/build.sh
+++ mcq-frontend/build.sh
@@ -1,5 +1,8 @@
 git clone https://github.com/tadevi/mcq
-cd mcq/mcq-frontend/
+cd mcq/
+git checkout frontend
+cd mcq-frontend/
+yarn install
 cd ckeditor5-build-decoupled-document && yarn link
 cd .. && yarn link "@ckeditor/ckeditor5-build-decoupled-document"
 yarn build
