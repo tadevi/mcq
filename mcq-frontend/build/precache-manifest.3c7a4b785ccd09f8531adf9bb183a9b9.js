fd0920f38b608ebb3c251c2bc9cb5872424730c1
Tuan Kiet <tuankiet.luong@foodycorp.vn>
2020-03-19 20:52:03 +0700

    Build product

---
+++ mcq-frontend/build/precache-manifest.3c7a4b785ccd09f8531adf9bb183a9b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74b4b3083a72f00cb05b55655db486f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b6a3ad4d55a9b18609a",
+    "url": "/static/css/main.5b61a9e5.chunk.css"
+  },
+  {
+    "revision": "91bbb838dabdf7dbb6d4",
+    "url": "/static/js/2.9ad81411.chunk.js"
+  },
+  {
+    "revision": "1b6a3ad4d55a9b18609a",
+    "url": "/static/js/main.55844f8c.chunk.js"
+  },
+  {
+    "revision": "98ca41096393d8622feb",
+    "url": "/static/js/runtime-main.dd99a021.js"
+  }
+]);
